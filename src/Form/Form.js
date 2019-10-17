33ee708211373576766e9050fa1dafdbc05f7963
Jon O'Drobinak <jodrobinak@gmail.com>
2019-10-17 12:34:40 -0600

    Update styling of input form, submit button

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -64,7 +64,7 @@ export default class Form extends Component {
         name='number'
         onChange={this.handleChange}
         />
-        <button 
+        <button className="submit-button"
           onClick={this.submitNewRes}
         >
           Submit Reservation
