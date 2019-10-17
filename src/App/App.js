df38bcc846961484da2933ce9a580def5da4b36a
Jon O'Drobinak <jodrobinak@gmail.com>
2019-10-17 11:31:22 -0600

    Linting, add input form for new reservations

--- src/App/App.js
+++ src/App/App.js
@@ -1,23 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import CardContainer from '../CardContainer/CardContainer';
-import Form from '../Form/Form'
-
+import Form from '../Form/Form';
 
 class App extends Component {
   constructor () {
     super();
     this.state = {
       reservations: []
-    
     }
   }
 
   componentDidMount = () => {
-    // const resData = () => {
       fetch('http://localhost:3001/api/v1/reservations')
       .then(response => response.json())
-      // .then(data => console.log('reserv is ', data))
       .then(reservations => {
         const reservationData = reservations.map(reservation => {
           let { id, name, date, time, number } = reservation;
@@ -27,7 +23,6 @@ class App extends Component {
       })
         .then(data => {
           return this.setReservations(data)})
- 
       .catch(error => console.log('error retrieving reservation: ', error.message))
   }
 
