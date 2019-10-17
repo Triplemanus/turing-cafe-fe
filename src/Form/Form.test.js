e1599e3dc212f3270b2a2490626c10d9d41170ab
Jon O'Drobinak <jodrobinak@gmail.com>
2019-10-17 12:08:19 -0600

    Add test in Form component

--- src/Form/Form.test.js
+++ src/Form/Form.test.js
@@ -16,12 +16,40 @@ describe('Form', () => {
 
   it('should update state when handleChange is called', () => {
     const mockEvent = { target: {  name: 'name', value: "Frankenstein"} };
+
     const expected = "Frankenstein";
 
     wrapper.instance().handleChange(mockEvent);
 
-    expect(wrapper.state('reservations')).toEqual(expected);
+    expect(wrapper.state('name')).toEqual(expected);
   });
 
+  it('should reset state to default when resetInputs method is called', () => {
+    const defaultState = { 
+      name: "Frank",
+      time: "24:00", 
+      date: "10/31",
+      number: "30"
+    };
+    
+    const expected = { name: '', time: '', date: '', number: '' };
+    
+    wrapper.instance().setState(defaultState);
+
+    wrapper.instance().resetInputs();
+
+    expect(wrapper.state()).toEqual(expected);
+  });
+
+  // it('should call addIdea and resetInputs when submitNewIdea is called', () => {
+  //   global.Date.now = jest.fn().mockImplementation(() => 12345)
+  //   const mockEvent = { preventDefault: jest.fn() };
+  //   const expected = { title: '', description: '', id: 12345 };
+  //   wrapper.instance().resetInputs = jest.fn();
+  //   wrapper.instance().submitNewIdea(mockEvent);
+    
+  //   expect(mockAddIdea).toHaveBeenCalledWith(expected);
+  //   expect(wrapper.instance().resetInputs).toHaveBeenCalled();
+  // });
 
 })
